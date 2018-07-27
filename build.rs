30b3a500848667f8747b27a0c35e3b6c1627cbe1
Artur Barnabas <kovacs.artur.barnabas@gmail.com>
2018-07-27 21:55:53 +0200

    Added icon for Windows.

---
+++ build.rs
@@ -0,0 +1,13 @@
+#[cfg(windows)]
+extern crate winres;
+
+#[cfg(windows)]
+fn main() {
+    let mut res = winres::WindowsResource::new();
+    res.set_icon("resource/emulsion.ico");
+    res.compile().unwrap();
+}
+
+#[cfg(unix)]
+fn main() {
+}
