c9e6bdc046c2a6e8a0a54f37341c4f3e80721d60
Artur Kovacs <kovacs.artur.barnabas@gmail.com>
2020-04-21 20:21:37 +0200

    Exculde the resources folder from the windows distribution.

--- distribution/windows/update_files.bat
+++ distribution/windows/update_files.bat
@@ -7,5 +7,4 @@ cargo build --release
 cd %distribution_dir%
 mkdir program
 copy /y ..\..\target\release\emulsion.exe program\emulsion.exe
-mkdir program\resource
-xcopy /s /y ..\..\target\release\resource program\resource
+
