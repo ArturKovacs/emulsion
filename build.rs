a39a37060f4edbe362612433d0344c5ed622506d
Tuncer Ayaz <tuncer.ayaz@gmail.com>
2020-04-28 01:28:11 +0000

    Remove unwrap calls in build.rs

--- build.rs
+++ build.rs
@@ -6,28 +6,32 @@ use std::fs;
 use std::path::Path;
 
 #[cfg(windows)]
-fn platform_specific() {
+fn platform_specific() -> std::io::Result<()> {
 	let mut res = winres::WindowsResource::new();
 	res.set_icon("resource_dev/emulsion.ico");
-	res.compile().unwrap();
+	res.compile()?;
+	Ok(())
 }
 
 #[cfg(unix)]
-fn platform_specific() {}
+fn platform_specific() -> std::io::Result<()> {
+	Ok(())
+}
 
-fn main() {
-	platform_specific();
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+	platform_specific()?;
 
 	let dir_name = "resource";
-	let profile = env::var("PROFILE").unwrap();
+	let profile = env::var("PROFILE")?;
 
 	let target_resource_path = Path::new("target").join(profile).join(dir_name);
-	fs::create_dir_all(target_resource_path.clone()).unwrap();
+	fs::create_dir_all(target_resource_path.clone())?;
 
-	for entry in fs::read_dir("resource/").unwrap() {
-		let entry = entry.unwrap();
-		if entry.file_type().unwrap().is_file() {
-			fs::copy(entry.path(), target_resource_path.join(entry.file_name())).unwrap();
+	for entry in fs::read_dir("resource/")? {
+		let entry = entry?;
+		if entry.file_type()?.is_file() {
+			fs::copy(entry.path(), target_resource_path.join(entry.file_name()))?;
 		}
 	}
+	Ok(())
 }
