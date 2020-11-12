d19b97540e8631f2ffa55933165bc292a446e024
hk <alexhaka10@protonmail.com>
2020-11-12 10:02:21 +0100

    fix: don't reset index to 0 on file deletion
    before this fix, the index would be set to 0 on 
    every file deletion

--- src/image_cache/directory.rs
+++ src/image_cache/directory.rs
@@ -215,10 +215,20 @@ impl Directory {
 	}
 
 	pub fn update_directory(&mut self) -> Result<()> {
-		let curr_filename = self.curr_filename();
+		let (curr_filename, curr_index) = (self.curr_filename(), self.curr_file_idx);
 		self.collect_directory()?;
+		// if possible preserve previous file_name
 		for (index, desc) in self.files.iter().enumerate() {
 			if desc.path.file_name().unwrap() == curr_filename {
+				self.curr_file_idx = index;
+				self.set_image_index_from_file_index();
+				self.check_filter_ready();
+				return Ok(());
+			}
+		}
+		// if is_file_supported, preserve index of previous file or its following files
+		for (index, desc) in self.files.iter().enumerate().skip(curr_index) {
+			if is_file_supported(&PathBuf::from(desc.path.file_name().unwrap())) {
 				self.curr_file_idx = index;
 				self.set_image_index_from_file_index();
 				self.check_filter_ready();
