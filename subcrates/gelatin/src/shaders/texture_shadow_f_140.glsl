158cc0dfcc39035ee8ddc4c680e250dfeed8a94c
Artur Kovacs <kovacs.artur.barnabas@gmail.com>
2021-04-05 13:18:57 +0200

    Fix shader compilation panic on macOS

--- subcrates/gelatin/src/shaders/texture_shadow_f_140.glsl
+++ subcrates/gelatin/src/shaders/texture_shadow_f_140.glsl
@@ -9,7 +9,7 @@ in vec2 v_tex_coords;
 out vec4 f_color;
 
 void main() {
-    vec4 color = texture2D(tex, v_tex_coords);
+    vec4 color = texture(tex, v_tex_coords);
     color = vec4(mix(color.rgb, vec3(1.0), max(0.0, brighten)), color.a);
     color = vec4(mix(color.rgb, vec3(0.0), -min(0.0, brighten)), color.a);
     color.rgb *= color.a;
